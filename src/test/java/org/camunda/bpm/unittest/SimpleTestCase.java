312ac57208a80c3e7acdacc11bff656079bb60a7
Christian Lipphardt <christian.lipphardt@googlemail.com>
2016-07-05 18:34:26 +0200

    related to SUPPORT-2492

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -12,20 +12,27 @@
  */
 package org.camunda.bpm.unittest;
 
+import org.camunda.bpm.engine.EntityTypes;
+import org.camunda.bpm.engine.history.UserOperationLogEntry;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Daniel Meyer
  * @author Martin Schimak
  */
 public class SimpleTestCase {
+
+  private static final Logger LOGGER = Logger.getLogger(SimpleTestCase.class.getName());
 
   @Rule
   public ProcessEngineRule rule = new ProcessEngineRule();
@@ -46,6 +53,24 @@ public class SimpleTestCase {
     complete(task(processInstance));
     // Then the process instance should be ended
     assertThat(processInstance).isEnded();
+
+    // get user ops
+    List<UserOperationLogEntry> list = rule.getHistoryService().createUserOperationLogQuery().list();
+
+    for (UserOperationLogEntry userOperationLogEntry : list) {
+      System.out.println(userOperationLogEntry.toString());
+    }
+    assertThat(list).hasSize(2);
+
+    boolean taskCreateFound = false;
+    for (UserOperationLogEntry userOperationLogEntry : list) {
+      if (userOperationLogEntry.getEntityType().equalsIgnoreCase(EntityTypes.TASK)) {
+        assertThat(userOperationLogEntry.getOperationType()).isEqualToIgnoringCase(UserOperationLogEntry.OPERATION_TYPE_CREATE);
+        taskCreateFound = true;
+      }
+    }
+
+    assertTrue("No create task user operation log entry found!", taskCreateFound);
   }
 
 }
